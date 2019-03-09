41b1205b5b0a7e13a669cf0829c864c4a67a57d3
Mercedes Vetráb <vetrab.mercedes@sonrisa.hu>
2019-03-09 21:21:13 +0100

    create gettimeofreachposition function test case for valid standard use case

--- src/main/java/ShipCollisionDetector/Models/Time.java
+++ src/main/java/ShipCollisionDetector/Models/Time.java
@@ -1,7 +1,32 @@
 package ShipCollisionDetector.Models;
 
-public class Time {
+import ShipCollisionDetector.Models.Enums.TimeUnit;
 
+public class Time {
+	TimeUnit timeUnit;
+	double timeValue;
+
+	public Time(TimeUnit timeUnit, double timeValue) {
+		this.timeUnit = timeUnit;
+		this.timeValue = timeValue;
+	}
+
+	public TimeUnit getTimeUnit() {
+		return timeUnit;
+	}
+
+	public void setTimeUnit(TimeUnit timeUnit) {
+		this.timeUnit = timeUnit;
+	}
+
+	public double getTimeValue() {
+		return timeValue;
+	}
+
+	public void setTimeValue(double timeValue) {
+		this.timeValue = timeValue;
+	}
+	
 	public Boolean isOverlap(Time otherTime) {
 		return null;
 	}
--- src/test/java/ShipCollisionDetector/Services/ShipInteractCalculatorTests.java
+++ src/test/java/ShipCollisionDetector/Services/ShipInteractCalculatorTests.java
@@ -14,54 +14,96 @@ import ShipCollisionDetector.Models.Mass;
 import ShipCollisionDetector.Models.Position;
 import ShipCollisionDetector.Models.Ship;
 import ShipCollisionDetector.Models.Speed;
+import ShipCollisionDetector.Models.Time;
 import ShipCollisionDetector.Models.Enums.LengthUnit;
 import ShipCollisionDetector.Models.Enums.MassUnit;
 import ShipCollisionDetector.Models.Enums.SpeedUnit;
+import ShipCollisionDetector.Models.Enums.TimeUnit;
 import ShipCollisionDetector.Service.Impl.ShipInteractCalculator;
 
 public class ShipInteractCalculatorTests {
 
 	private static double EPSYLON = 0.0001;
 	private ShipInteractCalculator shipInteractCalculator;
-	
-	public static Collection<Object[]> validDataSource() {
-		return Arrays.asList(new Object[][] { 
-			{ 
-				new Ship(
-						new Mass(MassUnit.KG, 2000),
-						new Length(LengthUnit.KM, 200),
-						new Speed(SpeedUnit.KMH, 30)
-						),
-				new Position(
-						new Length(LengthUnit.KM, 500),
-						new Length(LengthUnit.KM, 500)
-						),
-				270,
-				new Position(
-						new Length(LengthUnit.KM, 0),
-						new Length(LengthUnit.KM, 1169.345105)
-						)
-			} 
-		});
+
+	public static Collection<Object[]> validDataSourceForGetRoutesInteractPosition() {
+		return Arrays.asList(new Object[][] {{ 
+					new Ship(
+							new Mass(MassUnit.KG, 2000), 
+							new Length(LengthUnit.KM, 200), 
+							new Speed(SpeedUnit.KMH, 30)
+							),
+					new Position(
+							new Length(LengthUnit.KM, 500), 
+							new Length(LengthUnit.KM, 500)
+							),
+					270,
+					new Position(
+							new Length(LengthUnit.KM, 0),
+							new Length(LengthUnit.KM, 1169.345105)
+							)
+					}});
 	}
-	
+
+	public static Collection<Object[]> validDataSourceForGetTimeOfReachPosition() {
+		return Arrays.asList(new Object[][] {
+				{ 
+					new Ship(
+							new Mass(MassUnit.KG, 2000), 
+							new Length(LengthUnit.KM, 200), 
+							new Speed(SpeedUnit.KMH, 30)
+							),
+					new Position(
+							new Length(LengthUnit.KM, 500), 
+							new Length(LengthUnit.KM, 500)
+							), 
+					new Time(TimeUnit.H, 27.84925433)
+				},
+				{ 
+					new Ship(
+							new Mass(MassUnit.KG, 2000), 
+							new Length(LengthUnit.KM, 200), 
+							new Speed(SpeedUnit.KMH, 30)
+							),
+					new Position(
+							new Length(LengthUnit.KM, 0), 
+							new Length(LengthUnit.KM, 0)
+							), 
+					new Time(TimeUnit.H, 38.97817017)
+				}
+			});
+	}
+
 	@BeforeEach
 	void before() {
 		this.shipInteractCalculator = new ShipInteractCalculator();
 	}
 
 	@ParameterizedTest
-	@MethodSource("validDataSource")
+	@MethodSource("validDataSourceForGetRoutesInteractPosition")
 	void testGetRoutesInteractPosition(Ship interactShip, Position interactShipPosition, double interactShipDirection, Position expectedPosition) {
 		// Arrange
-		
-		//Act
+
+		// Act
 		Position calculatedPosition = shipInteractCalculator.getRoutesInteractPosition(interactShip, interactShipPosition, interactShipDirection);
-		
-		//Assert
+
+		// Assert
 		assertEquals(calculatedPosition.getX().getLengthValue(), expectedPosition.getX().getLengthValue(), EPSYLON);
 		assertThat(calculatedPosition.getX().getLengthUnit(), is(expectedPosition.getX().getLengthUnit()));
 		assertEquals(calculatedPosition.getY().getLengthValue(), expectedPosition.getY().getLengthValue(), EPSYLON);
 		assertThat(calculatedPosition.getY().getLengthUnit(), is(expectedPosition.getY().getLengthUnit()));
+	}
+
+	@ParameterizedTest
+	@MethodSource("validDataSourceForGetTimeOfReachPosition")
+	void testGetTimeOfReachPosition(Ship interactShip, Position interactShipPosition, Time expectedTime) {
+		// Arrange
+
+		// Act
+		Time calculatedTime = shipInteractCalculator.getTimeOfReachPosition(interactShip, interactShipPosition);
+
+		// Assert
+		assertEquals(calculatedTime.getTimeValue(), expectedTime.getTimeValue(), EPSYLON);
+		assertThat(calculatedTime.getTimeUnit(), is(expectedTime.getTimeUnit()));
 	}
 }
